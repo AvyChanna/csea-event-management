a24c4a25f1ae17b80fe3a69038afad699880e91a
priyanshu2103 <priyanshu.singh2k@gmail.com>
2019-04-15 19:31:27 +0530

    app feedback completely done

--- CSEAEventManagement/app/src/main/java/com/cseaeventmanagement/App_Feedback_Activity.java
+++ CSEAEventManagement/app/src/main/java/com/cseaeventmanagement/App_Feedback_Activity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
@@ -34,6 +35,7 @@ public class App_Feedback_Activity extends AppCompatActivity {
     private RatingBar ratingBar3;
     private float rating_overall;
     private String app_feeback_comment;
+    private Button submit_button;
     private EditText comments;
     private RequestQueue q;
 
@@ -69,78 +71,78 @@ public class App_Feedback_Activity extends AppCompatActivity {
             }
         });
 
-        View focusView = null;
-        boolean cancel = false;
+        submit_button = (Button) findViewById(R.id.btn_submit_app_feedback);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptFeedbackSubmit(rating_ui,rating_ux,rating_overall);
+            }
+        });
 
-        if(rating_ui==0)
+    }
+
+    public void attemptFeedbackSubmit(float rating_ui,float rating_ux,float rating_overall)
+    {
+        if(rating_ui==0||rating_ux==0||rating_overall==0)
         {
-            cancel = true;
-            focusView = ratingBar1;
+            Context context = getApplicationContext();
+            CharSequence text = "Fill all the entries first";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.show();
         }
-        if(rating_ux==0)
-        {
-            cancel = true;
-            focusView = ratingBar2;
-        }
-        if(rating_overall==0)
-        {
-            cancel = true;
-            focusView = ratingBar3;
-        }
-        if(cancel)
-            focusView.requestFocus();
         else
         {
             JSONObject obj = new JSONObject();
             comments = (EditText) findViewById(R.id.editText_app_comments);
             if(TextUtils.isEmpty(comments.getText().toString()))
                 app_feeback_comment = "";
-            else
+            else {
                 app_feeback_comment = comments.getText().toString();
-            try{
-                obj.accumulate("App_UI_Rating",rating_ui);
-                obj.accumulate("App_UX_Rating",rating_ux);
-                obj.accumulate("App_Overall_Rating",rating_overall);
-                obj.accumulate("App_Feedback_Comment",app_feeback_comment);
             }
-            catch (Exception e)
-            {
-                Log.d("APP_FEEDBACK_SUBMIT",e.toString());
-            }
-            JsonObjectRequest jor = new JsonObjectRequest(
-                    Request.Method.POST,
-                    "http://172.16.115.46:8000/api/login/",
-                    obj,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Log.d("API_FEEDBACK_SUBMIT", response.toString());
-                            Context context = getApplicationContext();
-                            CharSequence text = "Feedback successfully submitted";
-                            int duration = Toast.LENGTH_SHORT;
-                            Toast toast = Toast.makeText(context,text,duration);
-                            toast.show();
+                try{
+                    obj.accumulate("App_UI_Rating",rating_ui);
+                    obj.accumulate("App_UX_Rating",rating_ux);
+                    obj.accumulate("App_Overall_Rating",rating_overall);
+                    obj.accumulate("App_Feedback_Comment",app_feeback_comment);
+                }
+                catch (Exception e)
+                {
+                    Log.d("APP_FEEDBACK_SUBMIT",e.toString());
+                }
+                JsonObjectRequest jor = new JsonObjectRequest(
+                        Request.Method.POST,
+                        "http://172.16.115.46:8000/api/login/",
+                        obj,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("API_FEEDBACK_SUBMIT", response.toString());
+                                Context context = getApplicationContext();
+                                CharSequence text = "Feedback successfully submitted";
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context,text,duration);
+                                toast.show();
 //                            showProgress(false);
-                            // finish();
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Log.d("API_CALL_ERR_FEEDBACK", error.toString());
+                                // finish();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("API_CALL_ERR_FEEDBACK", error.toString());
 //                            showProgress(false);
-                            Snackbar.make(findViewById(R.id.request_event), "Error in submission. Check your network and try again", Snackbar.LENGTH_SHORT)
-                                    .setAction("Dismiss", new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
+                                Snackbar.make(findViewById(R.id.app_feedback), "Error in submission. Check your network and try again", Snackbar.LENGTH_SHORT)
+                                        .setAction("Dismiss", new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
 
-                                        }
-                                    }).show();
+                                            }
+                                        }).show();
+                            }
                         }
-                    }
-            );
-            q.add(jor);
+                );
+                q.add(jor);
+            }
         }
-
     }
-}
