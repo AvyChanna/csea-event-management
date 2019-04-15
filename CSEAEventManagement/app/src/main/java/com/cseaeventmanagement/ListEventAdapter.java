ddf58779375d28fe57b141999f2e2ca4a17f76bb
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2019-04-16 01:28:11 +0530

    Add list of events

--- CSEAEventManagement/app/src/main/java/com/cseaeventmanagement/ListEventAdapter.java
+++ CSEAEventManagement/app/src/main/java/com/cseaeventmanagement/ListEventAdapter.java
@@ -1,56 +1,62 @@
 package com.cseaeventmanagement;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ListEventAdapter extends RecyclerView.Adapter<ListEventAdapter.ViewHolder> {
-    //name
-    //date time
-    private List_Event_Data_POJO[] listdata;
 
-    // RecyclerView recyclerView;
-    public ListEventAdapter(List_Event_Data_POJO[] listdata) {
-        this.listdata = listdata;
-    }
+	private List_Event_Data_POJO[] listdata;
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View listItem = layoutInflater.inflate(R.layout.list_events_part, parent, false);
-        ViewHolder viewHolder = new ViewHolder(listItem);
-        return viewHolder;
-    }
+	// RecyclerView recyclerView;
+	public ListEventAdapter(List_Event_Data_POJO[] listdata) {
+		this.listdata = listdata;
+	}
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        final List_Event_Data_POJO List_Event_Data_POJO = listdata[position];
-        holder.textView.setText(listdata[position].getDescription());
-        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "click on item: " + List_Event_Data_POJO.getDescription(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+		View listItem = layoutInflater.inflate(R.layout.list_events_part, parent, false);
+		return  new ViewHolder(listItem);
+	}
 
-    @Override
-    public int getItemCount() {
-        return listdata.length;
-    }
+	@Override
+	public void onBindViewHolder(ViewHolder holder, int position) {
+		final List_Event_Data_POJO List_Event_Data_POJO = listdata[position];
+		holder.name.setText(listdata[position].eventName);
+		holder.date.setText(listdata[position].eventDate);
+		holder.desc.setText(listdata[position].eventDesc);
+		holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				//List_Event_Data_POJO.eventName
+				Intent intent = new Intent(view.getContext(), LoginActivity.class);
+				view.getContext().startActivity(intent);
+			}
+		});
+	}
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
-        public RelativeLayout relativeLayout;
+	@Override
+	public int getItemCount() {
+		return listdata.length;
+	}
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            this.textView = (TextView) itemView.findViewById(R.id.textView);
-            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relativeLayout);
-        }
-    }
+	public static class ViewHolder extends RecyclerView.ViewHolder {
+		public TextView name;
+		public TextView date;
+		public TextView desc;
+		public RelativeLayout relativeLayout;
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+			this.name = itemView.findViewById(R.id.name);
+			this.desc = itemView.findViewById(R.id.desc);
+			this.date = itemView.findViewById(R.id.date);
+			relativeLayout = itemView.findViewById(R.id.relativeLayout);
+		}
+	}
 }
