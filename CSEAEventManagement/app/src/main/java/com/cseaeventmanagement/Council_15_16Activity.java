3efe4b5b2f43248a079b50417f13ab391ea05a3d
Siddharth Agarwal <shagarwal@yahoo.com>
2019-04-15 14:58:43 +0530

    Hard Coded Council completed (Based on Master branch-login merged)

---
+++ CSEAEventManagement/app/src/main/java/com/cseaeventmanagement/Council_15_16Activity.java
@@ -0,0 +1,13 @@
+package com.cseaeventmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Council_15_16Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_council_15_16);
+    }
+}
